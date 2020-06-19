5d3197f1f33116a95419b0d4cf8f98e549559c2b
Jacob Grahn <jacob@grahn.io>
2020-06-19 16:57:17 -0400

    dump the liveness probe

---
+++ pipelines/canary-deploy/deployment/main.tf
@@ -0,0 +1,46 @@
+resource "kubernetes_deployment" "deployment" {
+  count = var.replicas > 0 ? 1 : 0
+
+  metadata {
+    name = var.name
+    labels = {
+      app = var.name
+    }
+  }
+
+  spec {
+    replicas = var.replicas
+
+    selector {
+      match_labels = {
+        app = var.name
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.name
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "nginx"
+
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
