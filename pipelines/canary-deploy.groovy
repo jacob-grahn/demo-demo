c0ab1648f01735816c1ed03f81a88efe5b977102
Jacob Grahn <jacob@grahn.io>
2020-06-19 15:27:41 -0400

    use jenkins credential

--- pipelines/canary-deploy.groovy
+++ pipelines/canary-deploy.groovy
@@ -1,40 +1,44 @@
 podTemplate {
     node(POD_LABEL) {
+        witchCredentials([
+            file(credentialsId: 'terraform-service-account', 'GOOGLE_APPLICATION_CREDENTIALS')
+        ]) {
 
-        stage('Checkout') {
-            checkout(scm)
-        }
-
-        stage('Auth') {
-            sh """
-                gcloud auth activate-service-account \
-                    --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-
-                gcloud container clusters get-credentials cicd \
-                    --region us-central1 \
-                    --project brightinsight-demo
-            """
-        }
-
-        dir('pipelines/canary-deploy') {
-            stage('Init') {
-                sh 'terraform init'
+            stage('Checkout') {
+                checkout(scm)
             }
 
-            stage('Deploy Canary') {
-                sh 'terraform apply'
+            stage('Auth') {
+                sh """
+                    gcloud auth activate-service-account \
+                        --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+
+                    gcloud container clusters get-credentials cicd \
+                        --region us-central1 \
+                        --project brightinsight-demo
+                """
             }
 
-            stage('Smoke Test Canary') {
-                print('Smoke tests passed!')
-            }
+            dir('pipelines/canary-deploy') {
+                stage('Init') {
+                    sh 'terraform init'
+                }
 
-            stage('Deploy Primary') {
-                sh 'terraform apply'
-            }
+                stage('Deploy Canary') {
+                    sh 'terraform apply'
+                }
 
-            stage('Tear Down Canary') {
-                sh 'terraform apply'
+                stage('Smoke Test Canary') {
+                    print('Smoke tests passed!')
+                }
+
+                stage('Deploy Primary') {
+                    sh 'terraform apply'
+                }
+
+                stage('Tear Down Canary') {
+                    sh 'terraform apply'
+                }
             }
         }
     }
